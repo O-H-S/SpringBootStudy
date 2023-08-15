176cfe28a6d816885f4c12910f1599d44367fa15
O-H-S <ohhyunsu0606@gmail.com>
2023-08-15 22:51:01 +0900

    [3-07]

--- src/main/java/com/example/demo/SecurityConfig.java
+++ src/main/java/com/example/demo/SecurityConfig.java
@@ -6,16 +6,30 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
 
-@Configuration
+@Configuration // 환경설정임을 의미
 @EnableWebSecurity // 모든 요청 URL이 스프링 시큐리티의 제어를 받도록 만드는 애너테이션이다. 내부적으로 SpringSecurityFilterChain이 동작하여 URL 필터가 적용된다.
 public class SecurityConfig 
 {
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception 
     {
-        http.authorizeHttpRequests().requestMatchers(new AntPathRequestMatcher("/**")).permitAll();
-        // 다음 문장은 모든 인증되지 않은 요청을 허락한다는 의미이다. 따라서 로그인을 하지 않더라도 모든 페이지에 접근할 수 있다.
+    	// 다음 문장은 모든 인증되지 않은 요청을 허락한다는 의미이다. 따라서 로그인을 하지 않더라도 모든 페이지에 접근할 수 있다.
+        http.authorizeHttpRequests().requestMatchers(new AntPathRequestMatcher("/**")).permitAll()
+        .and()
+		.csrf().ignoringRequestMatchers(
+				new AntPathRequestMatcher("/h2-console/**"))
+		.and()
+        .headers((headers) -> headers
+                .addHeaderWriter(new XFrameOptionsHeaderWriter(
+                    XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN)))  //스프링 시큐리티가 CSRF 처리시 H2 콘솔은 예외로 처리할 수 있도록
+        //스프링 시큐리티는 사이트의 콘텐츠가 다른 사이트에 포함되지 않도록 하기 위해 X-Frame-Options 헤더값을 사용하여 이를 방지한다. (clickjacking 공격을 막기위해 사용함)
+        .formLogin((formLogin) -> formLogin
+                .loginPage("/user/login")
+                .defaultSuccessUrl("/"))
+        ;
+       
         return http.build();
     }
     
